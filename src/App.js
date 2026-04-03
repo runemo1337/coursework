82dc84abdff7d02d5da711c091452f20902c0bbc
ivan5227 <grigorichev06@bk.ru>
2026-04-03 03:51:17 +0300

    add useState feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import Character from "./components/Characters";
 import './App.css';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  const handleCharacterClick = (characterName) => {
+    setSelectedCharacter(characterName);
+  };
+
+  if (selectedCharacter) {
+    return (
+      <div>
+        <h1>Диалог с {selectedCharacter}</h1>
+        <button onClick = {() => setSelectedCharacter(null)}>
+          Назад в офис
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>IT-офис</h1>
       <div className = "characters-grid">
-        <Character name = "Егор" role = "Backend"/>
-        <Character name = "Аня" role = "Frontend" />
-        <Character name = "Андрей" role = "DevOps" />
-        <Character name = "Вова" role = "Data science" />
-        <Character name = "Матвей" role = "Analytics" />
+        <Character name = "Егор" role = "Backend" onSelect = {handleCharacterClick} />
+        <Character name = "Аня" role = "Frontend" onSelect = {handleCharacterClick} />
+        <Character name = "Андрей" role = "DevOps" onSelect = {handleCharacterClick} />
+        <Character name = "Вова" role = "Data science" onSelect = {handleCharacterClick} />
+        <Character name = "Матвей" role = "Analytics" onSelect = {handleCharacterClick} />
       </div>
       
     </div>
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,6 +1,9 @@
 function Character(props) {
     return (
-        <div className = "characters-card">
+        <div 
+        className = "characters-card"
+        onClick = {() => props.onSelect(props.name)}
+        >
             <h3>{props.name}</h3>
             <p>{props.role}</p>
         </div>
